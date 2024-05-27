3bb7bbd2350e43d653f60279e9300a805aa1d2c3
Denis Fodor <fodor101@live.com>
2024-05-27 14:31:50 +0200

    Add names to call arguments in DataProvider

--- app/src/main/java/com/example/customgooglemapsmarkers/DataProvider.kt
+++ app/src/main/java/com/example/customgooglemapsmarkers/DataProvider.kt
@@ -6,36 +6,47 @@ class DataProvider {
     companion object {
         fun getMapMarkers(): List<MapMarkerObject> {
             val marker1 = MapMarkerObject(
-                1,
-                locationName = "Arena Centar", coordinates = LatLng(45.77, 15.93)
+                sequenceId = 1,
+                locationName = "Arena Centar",
+                coordinates = LatLng(45.77, 15.93)
             )
             val marker2 = MapMarkerObject(
-                2, completed = true,
+                sequenceId = 2,
+                completed = true,
                 category = Category.RESTOCK,
-                locationName = "Avenue Mall", coordinates = LatLng(45.77, 15.97)
+                locationName = "Avenue Mall",
+                coordinates = LatLng(45.77, 15.97)
             )
             val marker3 = MapMarkerObject(
-                0, depot = true,
-                locationName = "Ars Futura", coordinates = LatLng(45.79, 15.95)
+                sequenceId = 0,
+                depot = true,
+                locationName = "Ars Futura",
+                coordinates = LatLng(45.79, 15.95)
             )
             val marker4 = MapMarkerObject(
-                3,
+                sequenceId = 3,
                 delay = Delay.DELAY_AT_RISK,
-                locationName = "Supernova Garden Mall", coordinates = LatLng(45.83, 16.04)
+                locationName = "Supernova Garden Mall",
+                coordinates = LatLng(45.83, 16.04)
             )
             val marker5 = MapMarkerObject(
-                4,
+                sequenceId = 4,
                 delay = Delay.DELAY_LATE,
-                locationName = "City Center one West", coordinates = LatLng(45.79, 15.88)
+                locationName = "City Center one West",
+                coordinates = LatLng(45.79, 15.88)
             )
             val marker6 = MapMarkerObject(
-                5, completed = true,
-                locationName = "City Center one East", coordinates = LatLng(45.80, 16.05)
+                sequenceId = 5,
+                completed = true,
+                locationName = "City Center one East",
+                coordinates = LatLng(45.80, 16.05)
             )
             val marker7 = MapMarkerObject(
-                6, completed = true,
+                sequenceId = 6,
+                completed = true,
                 delay = Delay.DELAY_LATE,
-                locationName = "Westgate Shopping City", coordinates = LatLng(45.87, 15.82)
+                locationName = "Westgate Shopping City",
+                coordinates = LatLng(45.87, 15.82)
             )
             return listOf(marker1, marker2, marker3, marker4, marker5, marker6, marker7)
         }
